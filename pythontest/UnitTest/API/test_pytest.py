1f1d03764858bfb7ed0484fd93a948bcdf272c99
WWW-CPJ <1834638903@qq.com>
2025-01-13 16:22:45 +0800

    update pytest

--- pythontest/UnitTest/API/test_pytest.py
+++ pythontest/UnitTest/API/test_pytest.py
@@ -1,13 +1,11 @@
-# pytest 和 unittest 哪个更好用，哪个更广泛
 # pytest 和 unittest 都是 Python 中常用的单元测试框架。每个框架都有其优点和使用场景，选择使用哪个框架取决于你项目的需求、团队的习惯以及个人偏好。
-# 怎么选择用那种来进行测试
-# pytest 是一个功能强大、灵活的第三方测试框架，它支持 Python 2 和 Python 3。pytest 是社区非常推荐的测试框架，得到了广泛的使用。
 
-# 需要安装 pytest
+# Pytest is a third-party testing framework
 # pip install pytest
-# 运行测试
+
+# Run test
 # pytest test_pytest.py
-# 更详细的输出
+# We can add -v to the end of the command to get more information and output
 # pytest test_pytest.py -v
 
 import requests
@@ -18,6 +16,7 @@ def test_get_users():
     assert response.status_code == 200
     assert isinstance(response.json(), list)
     assert 'name' in response.json()[0]
+    # Use the assert mechanism to verify that the request is functional and that the response is as expected.
 
 def test_create_user():
     url = "http://jsonplaceholder.typicode.com/users"
