facc97df66f016ae6e03ae94d5cbdb0f7525757f
WWW-CPJ <1834638903@qq.com>
2025-02-14 16:58:36 +0800

    Integration Scrapy and Django

--- scrapyProject/dpcq/django_setup.py
+++ scrapyProject/dpcq/django_setup.py
@@ -1,7 +1,22 @@
+import sys
 import os
 import django
 
 def setup_django_env():
+
+    # 获取 Django 项目的绝对路径
+    django_project_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../search_project'))
+
+    # 将 Django项目路径添加到 Python 模块搜索路径中
+    sys.path.append(0, django_project_path)
+
     # 设置 Django 的配置文件路径
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'search_project.settings')
+
+    # 初始化 Django 环境
     django.setup()
+
+
+
+
+
--- scrapyProject/dpcq/dpcq/pipelines_chapters.py
+++ scrapyProject/dpcq/dpcq/pipelines_chapters.py
@@ -1,7 +1,18 @@
+import os
+import sys
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
+
+
 from itemadapter import ItemAdapter
 import sqlite3
-
 from dpcq.items import ChapterItem
+
+from django_setup import setup_django_env
+from search_project.search_app.models import SearchItem
+setup_django_env()
+
 
 class ChaptersPipeline:
     def __init__(self):
@@ -43,7 +54,11 @@ class ChaptersPipeline:
         try:
             if isinstance(item, ChapterItem):
                 for chapter_name, chapter_link in zip(item['chapter'], item['link']):
-                    # chapter = item.get('chapter', [])
+
+                    # 使用 Django 模型创建新纪录
+                    SearchItem.objects.create(chapter=chapter_name, link=chapter_link)
+
+                    # 使用 SQLite 原生 SQL 语句创建新纪录
                     self.cursor.execute('''
                                     INSERT INTO chapters (chapter, link)
                                     VALUES (?, ?)''', (chapter_name, chapter_link, ))
@@ -64,8 +79,11 @@ class ChaptersPipeline:
         except sqlite3.Error as e:
             spider.logger.error(f"Error inserting item into database:{e}")
             self.conn.rollback()
+            # 撤销当前事务中的所有操作，如果在事务中发生了错误（比如插入数据时遇到了问题），rollback()方法会撤销之前的所有更改。
             return None
     
+
+
         # if isinstance(chapter_item, ChapterItem):
         #     print ("分割线")
         #     try:
